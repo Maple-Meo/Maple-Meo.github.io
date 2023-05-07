7b9ee9a8288d9e086d534e85926d088abe851825
maplemeo <913439694@qq.com>
2023-05-07 15:18:42 +0800

    Site updated: 2023-05-07 15:18:40

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b1520/","posts/4a17b157/","posts/4a17b156/","posts/4a17b158/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["posts/4a17b1520/","posts/4a17b1551/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
